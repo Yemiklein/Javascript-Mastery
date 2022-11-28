e6bb566a05ef8ab82b7e8980ddf7f7c511f9ff94
Adeyemi <yemiklein77@gmail.com>
2022-11-28 19:34:30 +0100

    data types

--- starter/script.js
+++ starter/script.js
@@ -1,22 +1,25 @@
-let js = 'amazing'
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing'
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = 'yemrad';
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = 'yemrad';
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 
-let country = 'Nigeria'
-const continent = 'Africa'
-let population = 200000000
-const isIsland = false
-let language = 'English'
-let description = "Nigeria is in Africa, and it's 200000000 people speaks English"
+// let country = 'Nigeria'
+// const continent = 'Africa'
+// let population = 200000000
+// const isIsland = false
+// let language = 'English'
+// let description = "Nigeria is in Africa, and it's 200000000 people speaks English"
 
-console.log(country);
-console.log(continent);
-console.log(population);
-console.log(isIsland);
-console.log(language);
-console.log(description);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
+// console.log(description);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
