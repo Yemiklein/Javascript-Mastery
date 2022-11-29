31e29d898ac7319ba1e5c9a957b50c022a7be2dd
Adeyemi <yemiklein77@gmail.com>
2022-11-29 10:49:44 +0100

    template literals

--- starter/script.js
+++ starter/script.js
@@ -21,5 +21,24 @@
 // console.log(language);
 // console.log(description);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// let ageRaji = 2022 - 1986
+// let ageYemi = 2022 - 1966
+
+// console.log(ageRaji, ageYemi);
+
+// let firstName = 'Adeyemi'
+// let lastName = 'Raji'
+// console.log(firstName + ' ' + lastName);
+
+const firstName = 'Adeyemi'
+const job = 'Software Engineer'
+const birthYear = 1988;
+const currentYear = 2022
+
+const yemi = `I'm ${firstName} a ${currentYear - birthYear} year old ${job}!`
+console.log(yemi);
+console.log(`thanking God for life i can't explain`)
+console.log(`javascript string \n\ with multiple \n\ lines sucks`)
